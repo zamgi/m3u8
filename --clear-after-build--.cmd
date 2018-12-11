a1af7cca3e67738180ffb9edfcd6b369ee66fd6a
zamgi <denisov72@mail.ru>
2018-12-11 19:22:20 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -1,13 +1,13 @@
-rd ".vs" /S/Q
+rd ".vs" /S /Q
 
-rd "m3u8.client\bin" /S/Q
-rd "m3u8.client\obj" /S/Q
+rd "m3u8.client\bin" /S /Q
+rd "m3u8.client\obj" /S /Q
 
-rd "m3u8.console.app\bin" /S/Q
-rd "m3u8.console.app\obj" /S/Q
+rd "m3u8.console.app\bin" /S /Q
+rd "m3u8.console.app\obj" /S /Q
 
 del "m3u8.downloader\bin\*.pdb" /Q
-rd "m3u8.downloader\obj" /S/Q
+rd "m3u8.downloader\obj" /S /Q
 del "m3u8-chrome-extension\m3u8-downloader-host\bin\*.pdb" /Q
 
 rem pause
