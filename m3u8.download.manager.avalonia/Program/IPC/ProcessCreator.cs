52c8d15ced14f8ce4ce2bd53fd9fa3ab4b4972f3
zamgi <denisov72@mail.ru>
2020-03-10 00:42:14 +0300

    ...

--- m3u8.download.manager.avalonia/Program/IPC/ProcessCreator.cs
+++ m3u8.download.manager.avalonia/Program/IPC/ProcessCreator.cs
@@ -42,27 +42,27 @@ namespace m3u8.download.manager.ipc
         {
            public IntPtr hProcess;
            public IntPtr hThread;
-           public int dwProcessId;
-           public int dwThreadId;
+           public int    dwProcessId;
+           public int    dwThreadId;
         }
 
         [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Unicode)]
         private struct STARTUPINFO
         {
-             public Int32 cb;
+             public Int32  cb;
              public string lpReserved;
              public string lpDesktop;
              public string lpTitle;
-             public Int32 dwX;
-             public Int32 dwY;
-             public Int32 dwXSize;
-             public Int32 dwYSize;
-             public Int32 dwXCountChars;
-             public Int32 dwYCountChars;
-             public Int32 dwFillAttribute;
-             public Int32 dwFlags;
-             public Int16 wShowWindow;
-             public Int16 cbReserved2;
+             public Int32  dwX;
+             public Int32  dwY;
+             public Int32  dwXSize;
+             public Int32  dwYSize;
+             public Int32  dwXCountChars;
+             public Int32  dwYCountChars;
+             public Int32  dwFillAttribute;
+             public Int32  dwFlags;
+             public Int16  wShowWindow;
+             public Int16  cbReserved2;
              public IntPtr lpReserved2;
              public IntPtr hStdInput;
              public IntPtr hStdOutput;
