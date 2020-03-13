1bf458bd3a3239f7d7519b5b5f599e334f2f2a54
zamgi <denisov72@mail.ru>
2020-03-14 00:58:39 +0300

    ...

---
+++ m3u8.download.manager.avalonia/View/ParallelismForm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace m3u8.download.manager.ui
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class ParallelismForm : Window
+    {
+        #region [.ctor().]
+        public ParallelismForm()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load( this );
+        }
+
+        #endregion
+
+    }
+}
---
+++ m3u8.download.manager.avalonia/View/SettingsForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace m3u8.download.manager.ui
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class SettingsForm : Window
+    {
+        #region [.ctor().]
+        public SettingsForm()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load( this );
+        }
+
+        #endregion
+    }
+}
