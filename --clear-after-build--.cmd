28f23518d187436a7269c2628e44c42ae4799c30
zamgi <denisov72@mail.ru>
2020-03-16 20:59:25 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -13,13 +13,13 @@ rd "m3u8.downloader\bin" /S /Q
 rd "m3u8.downloader\obj" /S /Q
 del "m3u8-browser-extensions\_m3u8-downloader-host\m3u8.downloader.host\bin\*.pdb" /Q
 
-rd "m3u8.download.manager\bin" /S /Q
-rd "m3u8.download.manager\obj" /S /Q
-rd "m3u8.download.manager\.vs" /S /Q
+rd "m3u8.download.manager\WinForms\bin" /S /Q
+rd "m3u8.download.manager\WinForms\obj" /S /Q
+rd "m3u8.download.manager\WinForms\.vs" /S /Q
 del "m3u8-browser-extensions\_m3u8-downloader-host\m3u8.download.manager.host\bin\*.pdb" /Q
 
-rem rd "m3u8.download.manager.avalonia\bin" /S /Q
-rd "m3u8.download.manager.avalonia\obj" /S /Q
-rd "m3u8.download.manager.avalonia\.vs" /S /Q
+rem rd "m3u8.download.manager\Avalonia\bin" /S /Q
+rd "m3u8.download.manager\Avalonia\obj" /S /Q
+rd "m3u8.download.manager\Avalonia\.vs" /S /Q
 
 rem pause
