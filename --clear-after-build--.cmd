6e3f75f9bb6c5f706f3c8c246bfbb1ec5a05cdfc
zamgi <denisov72@mail.ru>
2021-11-02 20:31:17 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -9,6 +9,9 @@ rd "m3u8.client.tests\obj" /S /Q
 rd "m3u8.console.app\bin" /S /Q
 rd "m3u8.console.app\obj" /S /Q
 
+rd "m3u8.console.app_2\bin" /S /Q
+rd "m3u8.console.app_2\obj" /S /Q
+
 rd "m3u8.downloader\bin" /S /Q
 rd "m3u8.downloader\obj" /S /Q
 del "m3u8-browser-extensions\_m3u8-downloader-host\m3u8.downloader.host\bin\*.pdb" /Q
