e52451fcbe242f9384bd2e25fcd3fa72c7db38a0
zamgi <denisov72@mail.ru>
2021-11-18 22:43:50 +0300

    m3u8_live_stream_downloader

---
+++ m3u8_live_stream_downloader/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace m3u8
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class Program
+    {
+        private static async Task Main( string[] args )
+        {
+            try
+            {
+                var M3U8_URL                   = ConfigurationManager.AppSettings[ "M3U8_URL" ];
+                var OUTPUT_FILENAME            = ConfigurationManager.AppSettings[ "OUTPUT_FILENAME" ];
+                var MAX_OUTPUT_FILE_SZIE_IN_MB = (int.TryParse( ConfigurationManager.AppSettings[ "MAX_OUTPUT_FILE_SZIE_IN_MB" ], out var i ) ? i : 250) * 1024 * 1024;
+                //-------------------------------------------------------------------//
+
+                using var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => cts.Cancel( e );
+                //-------------------------------------------------------------------//
+
+                $"Start download from M3U8_URL: '{M3U8_URL}'".ToConsole( to_title: true );
+
+                var p = new m3u8_live_stream_downloader.InitParams()
+                { 
+                    M3u8Url        = M3U8_URL,
+                    OutputFileName = OUTPUT_FILENAME,
+
+                    DownloadContent      = (p)                        => $"[ QUEUEED]: {p}".ToConsole( cts ),
+                    DownloadContentError = (p, ex)                    => $"[ QUEUEED]: {p} => {ex}".ToConsole( ConsoleColor.Red, cts ),
+                    DownloadPart         = (p, partBytes, totalBytes) => $"[DOWNLOAD]: {p} => ok. (part-size: {(1.0 * partBytes / 1024):N2} KB, total-size: {(1.0 * totalBytes / (1024 * 1024)):N2} MB)".ToConsole( cts ),
+                    DownloadPartError    = (p, ex)                    => $"[DOWNLOAD]: {p} => {ex}".ToConsole( ConsoleColor.Red, cts ),
+                    DownloadCreateOutputFile = (fn)                   => $"Created output file: '{fn}'".ToConsole( cts )
+                };
+
+                using var m = new m3u8_live_stream_downloader( in p );
+                await m.Download( cts.Token, MAX_OUTPUT_FILE_SZIE_IN_MB ).CAX();
+                //-------------------------------------------------------------------//
+            }
+            catch ( Exception ex )
+            {
+                ex.ToConsole();
+            }
+            //-----------------------------------------------------------//
+            $"\r\n[.....finita.....]".ToConsole( ConsoleColor.DarkGray, to_title: true );
+            Console.ReadLine();
+        }        
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class Extensions
+    {
+        public static void Cancel( this CancellationTokenSource cts, ConsoleCancelEventArgs e )
+        {
+            e.Cancel = true;
+            cts.Cancel();
+        }
+
+        public static void ToConsole( this string msg, CancellationTokenSource cts = null, bool to_title = false )
+        {
+            if ( (cts == null) || !cts.IsCancellationRequested )
+            {
+                Console.WriteLine( msg );
+                if ( to_title ) Console.Title = msg;
+            }
+        }
+        public static void ToConsole( this string msg, ConsoleColor foregroundColor, CancellationTokenSource cts = null, bool to_title = false )
+        {
+            if ( (cts == null) || !cts.IsCancellationRequested )
+            {
+                var fc = Console.ForegroundColor;
+                Console.ForegroundColor = foregroundColor;
+                Console.WriteLine( msg );
+                Console.ForegroundColor = fc;
+                if ( to_title ) Console.Title = msg;
+            }
+        }
+        public static void ToConsole( this Exception ex ) => ex.ToString().ToConsole( ConsoleColor.Red );
+    }
+}
---
+++ m3u8_live_stream_downloader/m3u8_live_stream_downloader.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace m3u8
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal sealed class m3u8_live_stream_downloader : IDisposable
+    {
+        public delegate void DownloadContentErrorDelegate( string m3u8_url, Exception ex );
+        public delegate void DownloadContentDelegate( string part_url );
+        public delegate void DownloadPartDelegate( string part_url, long part_size_in_bytes, long total_in_bytes );
+        public delegate void DownloadPartErrorDelegate( string part_url, Exception ex );
+        public delegate void DownloadCreateOutputFileDelegate( string output_file_name );
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public struct InitParams
+        {
+            public string M3u8Url        { get; set; }
+            public string OutputFileName { get; set; }
+
+            public HttpClient HttpClient { get; set; }
+
+            public DownloadContentErrorDelegate     DownloadContentError     { get; set; }
+            public DownloadContentDelegate          DownloadContent          { get; set; }
+            public DownloadPartDelegate             DownloadPart             { get; set; }
+            public DownloadPartErrorDelegate        DownloadPartError        { get; set; }
+            public DownloadCreateOutputFileDelegate DownloadCreateOutputFile { get; set; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private enum PartUrlStatusEnum
+        {
+            Queueed,
+            Processed,
+        }
+
+        private HttpClient _HttpClient;
+        private bool _Dispose_HttpClient;
+        private InitParams _IP;
+        private BlockingCollection< string > _PartUrls;
+        private Dictionary< string, PartUrlStatusEnum > _PartUrlsStatus;
+        public m3u8_live_stream_downloader( in InitParams ip )
+        {
+            M3u8Url        = ip.M3u8Url        ?? throw (new ArgumentNullException( nameof(ip.M3u8Url) ));
+            OutputFileName = ip.OutputFileName ?? throw (new ArgumentNullException( nameof(ip.OutputFileName) ));
+
+            _IP = ip;
+
+            _HttpClient         = ip.HttpClient ?? new HttpClient();
+            _Dispose_HttpClient = (ip.HttpClient == null);
+            _PartUrls           = new BlockingCollection< string >();
+            _PartUrlsStatus     = new Dictionary< string, PartUrlStatusEnum >();
+        }
+        public static async Task _Download_( string m3u8_url, string output_file_name, CancellationToken ct, int? max_output_file_size, int milliseconds_delay_between_request = 1_000 )
+        {
+            using var m = new m3u8_live_stream_downloader( new InitParams() { M3u8Url = m3u8_url, OutputFileName = output_file_name } );
+            await m.Download( ct, max_output_file_size, milliseconds_delay_between_request ).CAX();
+        }
+        public static async Task _Download_( InitParams ip, CancellationToken ct, int? max_output_file_size, int milliseconds_delay_between_request = 1_000 )
+        {
+            using var m = new m3u8_live_stream_downloader( in ip );
+            await m.Download( ct, max_output_file_size, milliseconds_delay_between_request ).CAX();
+        }
+        public void Dispose()
+        {
+            if ( _Dispose_HttpClient )
+            {
+                _HttpClient.Dispose();
+            }
+        }
+
+        public string M3u8Url        { get; private set; }
+        public string OutputFileName { get; private set; }
+
+        public async Task Download( CancellationToken ct, int? max_output_file_size, int milliseconds_delay_between_request = 1_000 )
+        {
+            var m3u8_url         = _IP.M3u8Url;
+            var output_file_name = _IP.OutputFileName;
+
+            var i = m3u8_url.LastIndexOf( '/' );
+            var base_url = (i != -1) ? m3u8_url.Substring( 0, i + 1 ) : m3u8_url;
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            var task_DownloadContent = RunDownloadContent( m3u8_url, ct, milliseconds_delay_between_request );
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task task_DownloadParts;
+            if ( max_output_file_size.HasValue )
+            {
+                task_DownloadParts = RunDownloadParts_WithMaxFileSize( output_file_name, base_url, ct, max_output_file_size.Value );
+            }
+            else
+            {
+                task_DownloadParts = RunDownloadParts( output_file_name, base_url, ct );
+            }            
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            //---await Task.Delay( Timeout.Infinite, ct ).CAX();
+            var task = await Task.WhenAny( task_DownloadContent, task_DownloadParts ).CAX();
+            task.GetAwaiter().GetResult();
+            //---await task.ContinueWith( t => throw t.Exception, TaskContinuationOptions.OnlyOnFaulted ).CAX();
+        }
+
+        private Task RunDownloadContent( string m3u8_url, CancellationToken ct, int milliseconds_delay )
+            => Task.Run( async () =>
+            {
+                //var debug_hs = new HashSet< string >();
+
+                for ( var hs = new HashSet< string >(); !ct.IsCancellationRequested; )
+                {
+                    try
+                    {
+                        var content = await _HttpClient.GetStringAsync( m3u8_url, ct ).CAX();
+
+                        var parts = content?.Split().Where( s => !s.StartsWith( "#" ) )
+                                                    .Select( s => s.Trim() )
+                                                    .Where( s => !s.IsNullOrEmpty() )
+                                                    .ToHashSet( hs );
+                        if ( parts != null )
+                        {
+                            var new_parts = 0;
+                            foreach ( var p in parts )
+                            {
+                                if ( _PartUrlsStatus.TryAddWithLock( p, PartUrlStatusEnum.Queueed ) )
+                                {
+                                    //var suc = debug_hs.Add( p ); Debug.Assert( suc );
+
+                                    _PartUrls.Add( p );
+                                    _IP.DownloadContent?.Invoke( p );
+                                    new_parts++;
+                                }
+                            }
+
+                            if ( new_parts != 0 )
+                            {
+                                _PartUrlsStatus.RemoveWhereValuesWithLock( p => !parts.Contains( p ), status => status == PartUrlStatusEnum.Processed );
+                            }
+                        }
+                        else
+                        {
+                            _IP.DownloadContent?.Invoke( "[empty content]" );
+                        }
+                    }
+                    catch ( Exception ex )
+                    {
+                        _IP.DownloadContentError?.Invoke( m3u8_url, ex );
+                    }
+
+                    await Task.Delay( milliseconds_delay, ct ).CAX();
+                }
+            }
+            , ct );
+        private Task RunDownloadParts( string output_file_name, string base_url, CancellationToken ct )
+            => Task.Run( async () =>
+            {
+                var dir = Path.GetDirectoryName( output_file_name );
+                if ( !Directory.Exists( dir ) ) Directory.CreateDirectory( dir );
+
+                using var fs = new FileStream( output_file_name, FileMode.Create, FileAccess.Write, FileShare.Read );
+                _IP.DownloadCreateOutputFile?.Invoke( output_file_name );
+
+                //var debug_lst = new List< string >();
+                long totalBytes = 0;
+                var fi = new FileInfo( output_file_name );
+                for ( var n = 0; !ct.IsCancellationRequested; )
+                {
+                    var p = _PartUrls.Take( ct );
+                    var part_url = base_url + p;
+                    
+                    try
+                    {
+                        //debug_lst.Add( p );
+                        Debug.WriteLine( p );
+
+                        using var s = await _HttpClient.GetStreamAsync( part_url, ct ).CAX();
+                        await s.CopyToAsync( fs/*, ct */).CAX();
+
+                        if ( (++n % 10) == 0 )
+                        {
+                            await s.FlushAsync( /*, ct */).CAX();
+                        }
+
+                        fi.Refresh();
+                        var file_size = fi.Length;
+                        var partBytes = file_size - totalBytes;
+                        totalBytes = file_size;
+                        //var partBytes = fi.Length;
+                        //totalBytes += partBytes;
+                        _IP.DownloadPart?.Invoke( p, partBytes, totalBytes );
+                    }
+                    catch ( Exception ex )
+                    {
+                        _IP.DownloadPartError?.Invoke( p, ex );
+                    }
+                    finally
+                    {
+                        _PartUrlsStatus.UpdateWithLock( p, PartUrlStatusEnum.Processed );
+                    }
+                }
+            }
+            , ct );
+        private Task RunDownloadParts_WithMaxFileSize( string output_file_name, string base_url, CancellationToken ct, int max_output_file_size )
+            => Task.Run( async () =>
+            {
+                var dir = Path.GetDirectoryName( output_file_name );
+                if ( !Directory.Exists( dir ) ) Directory.CreateDirectory( dir );
+
+                var fileNumber = 0;
+                string get_next_output_file_name() => Path.Combine( dir, Path.GetFileNameWithoutExtension( output_file_name ) + $"-({++fileNumber})" + Path.GetExtension( output_file_name ) );
+
+                long totalBytes = 0;
+                var fi = default(FileInfo);
+                Stream open_file_stream_4_write()
+                {
+                    var fn = get_next_output_file_name();
+                    var fs = new FileStream( fn, FileMode.Create, FileAccess.Write, FileShare.Read );
+                    _IP.DownloadCreateOutputFile?.Invoke( fn );
+                    totalBytes = 0;
+                    fi = new FileInfo( fn );
+                    return (fs);
+                }
+
+            NEXT_FILE:
+                using ( var fs = open_file_stream_4_write() )
+                {
+                    for ( var n = 0; !ct.IsCancellationRequested; )
+                    {
+                        var p = _PartUrls.Take( ct );
+                        var part_url = base_url + p;
+                    
+                        try
+                        {
+                            Debug.WriteLine( p );
+
+                            using var s = await _HttpClient.GetStreamAsync( part_url, ct ).CAX();
+                            await s.CopyToAsync( fs/*, ct */).CAX();
+
+                            if ( (++n % 10) == 0 )
+                            {
+                                await s.FlushAsync( /*, ct */).CAX();
+                            }
+
+                            fi.Refresh();
+                            var file_size = fi.Length;
+                            var partBytes = file_size - totalBytes;
+                            totalBytes = file_size;
+
+                            _IP.DownloadPart?.Invoke( p, partBytes, totalBytes );
+
+                            if ( max_output_file_size <= totalBytes )
+                            {
+                                goto NEXT_FILE;
+                            }
+                        }
+                        catch ( Exception ex )
+                        {
+                            _IP.DownloadPartError?.Invoke( p, ex );
+                        }
+                        finally
+                        {
+                            _PartUrlsStatus.UpdateWithLock( p, PartUrlStatusEnum.Processed );
+                        }
+                    }
+                }
+            }
+            , ct );
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class m3u8_live_stream_downloader_extensions
+    {
+        public static bool IsNullOrEmpty( this string s ) => string.IsNullOrEmpty( s );
+        public static bool IsNullOrWhiteSpace( this string s ) => string.IsNullOrWhiteSpace( s );
+
+        public static ConfiguredTaskAwaitable< T > CAX< T >( this Task< T > task ) => task.ConfigureAwait( false );
+        public static ConfiguredTaskAwaitable CAX( this Task task ) => task.ConfigureAwait( false );
+
+        public static HashSet< T > ToHashSet< T >( this IEnumerable< T > seq, HashSet< T > hs )
+        {
+            hs.Clear();
+            if ( seq != null )
+            {
+                foreach ( var t in seq )
+                {
+                    hs.Add( t );
+                }
+            }
+            return (hs);
+        }
+        public static bool TryAddWithLock< K, T >( this Dictionary< K, T > dict, K k, T t )
+        {
+            lock ( dict )
+            {
+                if ( !dict.ContainsKey( k ) )
+                {
+                    dict.Add( k, t );
+                    return (true);
+                }
+            }
+            return (false);
+        }
+        public static bool UpdateWithLock< K, T >( this Dictionary< K, T > dict, K k, T t )
+        {
+            lock ( dict )
+            {
+                if ( dict.ContainsKey( k ) )
+                {
+                    dict[ k ] = t;
+                    return (true);
+                }
+                //dict[ k ] = t;
+            }
+            return (false);
+        }
+        public static void RemoveWhereValuesWithLock< K, T >( this Dictionary< K, T > dict, Func< K, bool > keyMatchFunc, Func< T, bool > valueMatchFunc
+            , List< KeyValuePair< K, T > > buf = null )
+        {
+            lock ( dict )
+            {
+                if ( buf == null ) buf = new List< KeyValuePair< K, T > >( dict.Count );
+                else buf.Clear();
+
+                buf.AddRange( dict );
+                foreach ( var p in buf )
+                {
+                    if ( keyMatchFunc( p.Key ) && valueMatchFunc( p.Value ) )
+                    {
+                        dict.Remove( p.Key );
+                    }
+                }
+            }
+        }
+    }
+}
