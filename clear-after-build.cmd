d795de5cd6bc90dee9bb92b05e2c0ff12fb6b0f5
zamgi <denisov72@mail.ru>
2021-12-01 21:07:18 +0300

    ...

---
+++ clear-after-build.cmd
@@ -0,0 +1,36 @@
+rd ".vs" /S /Q
+
+rd "m3u8.client\bin" /S /Q
+rd "m3u8.client\obj" /S /Q
+
+rd "m3u8.client.tests\bin" /S /Q
+rd "m3u8.client.tests\obj" /S /Q
+
+rd "m3u8.console.app\bin" /S /Q
+rd "m3u8.console.app\obj" /S /Q
+
+rd "m3u8.console.app_2\bin" /S /Q
+rd "m3u8.console.app_2\obj" /S /Q
+
+rd "m3u8.downloader\bin" /S /Q
+rd "m3u8.downloader\obj" /S /Q
+del "m3u8-browser-extensions\_m3u8-downloader-host\m3u8.downloader.host\bin\*.pdb" /Q
+
+rd "m3u8.download.manager\WinForms\bin" /S /Q
+rd "m3u8.download.manager\WinForms\obj" /S /Q
+rd "m3u8.download.manager\WinForms\.vs" /S /Q
+del "m3u8-browser-extensions\_m3u8-downloader-host\m3u8.download.manager.host\bin\*.pdb" /Q
+
+rd "m3u8.download.manager\Avalonia\bin" /S /Q
+rd "m3u8.download.manager\Avalonia\obj" /S /Q
+rd "m3u8.download.manager\Avalonia\.vs" /S /Q
+
+rd "m3u8.download.manager\WinForms.NET50\bin" /S /Q
+rd "m3u8.download.manager\WinForms.NET50\obj" /S /Q
+rd "m3u8.download.manager\WinForms.NET50\.vs" /S /Q
+
+rd "m3u8_live_stream_downloader\bin" /S /Q
+rd "m3u8_live_stream_downloader\obj" /S /Q
+rd "m3u8_live_stream_downloader\.vs" /S /Q
+
+rem pause
