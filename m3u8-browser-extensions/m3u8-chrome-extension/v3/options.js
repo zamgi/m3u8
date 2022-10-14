d16b699e92adb3a4b8fd5d20a55983b74c2d9211
zamgi <denisov72@mail.ru>
2022-10-14 18:55:12 +0300

    ...

--- m3u8-browser-extensions/m3u8-chrome-extension/v3/options.js
+++ m3u8-browser-extensions/m3u8-chrome-extension/v3/options.js
@@ -1,12 +1,15 @@
 window.onload = async function () {
-    //---console.log('start \'m3u8_options.html\': ' + new Date().toLocaleString());
-
     let res = await chrome.storage.local.get();
+
     let ch = document.getElementById('saveUrlListBetweenTabReload');
     ch.checked = !!res.saveUrlListBetweenTabReload;
     ch.addEventListener('click', async function (event) {
-        //---console.log('this.checked \'m3u8_options.html\': ' + this.checked);
-
         await chrome.storage.local.set({ saveUrlListBetweenTabReload: this.checked });
+    });
+
+    ch = document.getElementById('directionRtl');
+    ch.checked = (res.directionRtl !== undefined) ? !!res.directionRtl : ch.checked;
+    ch.addEventListener('click', async function (event) {
+        await chrome.storage.local.set({ directionRtl: this.checked });
     });
 };
