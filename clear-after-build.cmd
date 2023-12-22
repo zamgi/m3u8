01f9dad7e98a239728d01e0ac452b959a8cdb4c6
zamgi <34erER34>
2023-12-22 20:25:24 +0300

    ...

--- clear-after-build.cmd
+++ clear-after-build.cmd
@@ -1,5 +1,9 @@
 rd ".vs" /S /Q
 
+rem for /f "usebackq" %%f in (`"dir /ad/b/s bin"`) do rd "%%f" /S/Q
+for /f "usebackq" %%f in (`"dir /ad/b/s obj"`) do rd "%%f" /S/Q
+for /f "usebackq" %%f in (`"dir /ad/b/s .vs"`) do rd "%%f" /S/Q
+
 rd "m3u8.client\bin" /S /Q
 rd "m3u8.client\obj" /S /Q
 
@@ -12,8 +16,8 @@ rd "m3u8.console.app\obj" /S /Q
 rd "m3u8.console.app_2\bin" /S /Q
 rd "m3u8.console.app_2\obj" /S /Q
 
-rd "m3u8.downloader\bin" /S /Q
-rd "m3u8.downloader\obj" /S /Q
+rd "prev\m3u8.downloader\bin" /S /Q
+rd "prev\m3u8.downloader\obj" /S /Q
 del "m3u8-browser-extensions\_m3u8-downloader-host\m3u8.downloader.host\bin\*.pdb" /Q
 
 rd "m3u8.download.manager\WinForms\bin" /S /Q
