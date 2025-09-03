9f3bd62ab22b51f9813656e522f6e40afb672cf0
zamgi <34erER34>
2025-09-03 16:02:14 +0300

    ...

--- m3u8.download.manager/WinForms/UI/Forms/MainForm.cs
+++ m3u8.download.manager/WinForms/UI/Forms/MainForm.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Security.Policy;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
