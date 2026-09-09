f7399239a7cfa255cd2af28e51077271c4996113
zamgi <34erER34>
2026-09-09 03:54:59 +0300

    ...

--- clear-after-build.cmd
+++ clear-after-build.cmd
@@ -15,11 +15,6 @@ rd "m3u8.client.tests\obj" /S /Q
 rd "m3u8.console.app\bin" /S /Q
 rd "m3u8.console.app\obj" /S /Q
 
-rd "m3u8.console.app_2\bin" /S /Q
-rd "m3u8.console.app_2\obj" /S /Q
-
-rd "prev\m3u8.downloader\bin" /S /Q
-rd "prev\m3u8.downloader\obj" /S /Q
 del "m3u8-browser-extensions\_m3u8-downloader-host\m3u8.downloader.host\bin\*.pdb" /Q
 
 rd "m3u8.download.manager\WinForms\bin" /S /Q
@@ -30,10 +25,6 @@ del "m3u8-browser-extensions\_m3u8-downloader-host\m3u8.download.manager.host\bi
 rd "m3u8.download.manager\Avalonia\bin" /S /Q
 rd "m3u8.download.manager\Avalonia\obj" /S /Q
 rd "m3u8.download.manager\Avalonia\.vs" /S /Q
-
-rd "m3u8.download.manager\WinForms.NET_CORE\bin" /S /Q
-rd "m3u8.download.manager\WinForms.NET_CORE\obj" /S /Q
-rd "m3u8.download.manager\WinForms.NET_CORE\.vs" /S /Q
 
 rd "m3u8_live_stream_downloader\bin" /S /Q
 rd "m3u8_live_stream_downloader\obj" /S /Q
